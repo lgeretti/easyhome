6cf8beb95a3d45a296386ebb4e45c4812eb3b3b7
Luca Geretti <luca.geretti@gmail.com>
2012-09-01 20:27:42 +0200

    Fixed test on hub

--- rest/src/test/java/it/uniud/easyhome/rest/HubResourceTest.java
+++ rest/src/test/java/it/uniud/easyhome/rest/HubResourceTest.java
@@ -2,15 +2,17 @@ package it.uniud.easyhome.rest;
 
 import static org.junit.Assert.*;
 
-import it.uniud.easyhome.gateway.Gateway;
+import it.uniud.easyhome.gateway.GatewayInfo;
 import it.uniud.easyhome.gateway.ProtocolType;
 import it.uniud.easyhome.testutils.JsonJaxbContextResolver;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.client.Configuration;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Form;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedHashMap;
@@ -21,13 +23,12 @@ import javax.ws.rs.core.Response.Status;
 import org.junit.*;
 
 import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.glassfish.jersey.media.json.JsonJaxbFeature;
 import org.glassfish.jersey.media.json.JsonJaxbModule;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 
-public class GatewayResourceTest extends JerseyTest {
+public class HubResourceTest extends JerseyTest {
 
     private static final String[] PACKAGE_NAMES = {
         "it.uniud.easyhome.gateway",
@@ -55,47 +56,47 @@ public class GatewayResourceTest extends JerseyTest {
     @Test
     public void testNoGateways() {
         
-        GenericType<List<Gateway>> gatewaysType = new GenericType<List<Gateway>>() {};
-        List<Gateway> gateways = target().path("gateways").request(MediaType.APPLICATION_JSON).get(gatewaysType);
+        GenericType<List<GatewayInfo>> gatewaysType = new GenericType<List<GatewayInfo>>() {};
+        List<GatewayInfo> gateways = target().path("hub/gateways").request(MediaType.APPLICATION_JSON).get(gatewaysType);
         assertEquals(0,gateways.size());
     }
 
     @Test
-    public void testInsertion() throws JSONException {
+    public void testInsertion() throws JSONException, IOException {
         
         MultivaluedMap<String,String> formData = new MultivaluedHashMap<String,String>();
         formData.add("port","5000");
-        formData.add("protocol",ProtocolType.ZIGBEE.toString());
+        formData.add("protocol",ProtocolType.XBEE.toString());
         
-        Response insertionResponse = target().path("gateways").request().post(Entity.form(formData));
+        Response insertionResponse = target().path("hub/gateways").request().post(Entity.form(formData));
         assertEquals(Status.CREATED,insertionResponse.getStatusInfo());
         
-        GenericType<List<Gateway>> gatewaysType = new GenericType<List<Gateway>>() {};
-        List<Gateway> gateways = target().path("gateways").request(MediaType.APPLICATION_JSON).get(gatewaysType);
+        GenericType<List<GatewayInfo>> gatewaysType = new GenericType<List<GatewayInfo>>() {};
+        List<GatewayInfo> gateways = target().path("hub/gateways").request(MediaType.APPLICATION_JSON).get(gatewaysType);
         assertEquals(1,gateways.size());
-        Gateway gw = gateways.get(0);
+        GatewayInfo gw = gateways.get(0);
         assertTrue(gw.getId()>0);
         assertEquals(5000,gw.getPort());
-        assertEquals(ProtocolType.ZIGBEE,gw.getProtocolType());
+        assertEquals(ProtocolType.XBEE,gw.getProtocolType());
     }
 
-    @Ignore
     @Test
     public void testDelete() {
         
         MultivaluedMap<String,String> formData = new MultivaluedHashMap<String,String>();
         formData.add("port","2000");
-        formData.add("protocol",ProtocolType.EHS.toString());
-        target().path("gateways").request().post(Entity.form(formData));
+        formData.add("protocol",ProtocolType.XBEE.toString());
+        Response insertionResponse = target().path("hub/gateways").request().post(Entity.form(formData));
+        URI location = insertionResponse.getLocation();
         
-        Response deleteResponse1 = target().path("gateways/1").request().delete();
+        Response deleteResponse1 = target().path(location.getPath()).request().delete();
         assertEquals(Status.OK,deleteResponse1.getStatusInfo());
-        Response deleteResponse2 = target().path("gateways/1").request().delete();
+        Response deleteResponse2 = target().path(location.getPath()).request().delete();
         assertEquals(Status.NOT_FOUND,deleteResponse2.getStatusInfo());
     }
     
     @After
     public void clear() {
-        target().path("gateways").request().delete();
+        target().path("hub/gateways").request().delete();
     }
 }
