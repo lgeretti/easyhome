50caaf4faabeed4d69d1cc0cf55166d2d457b788
Luca Geretti <luca.geretti@gmail.com>
2013-01-08 20:24:53 +0100

    Added toy example of push process

--- rest/src/main/java/it/uniud/easyhome/jsf/NodesController.java
+++ rest/src/main/java/it/uniud/easyhome/jsf/NodesController.java
@@ -7,31 +7,38 @@ import it.uniud.easyhome.network.NetworkEJB;
 import it.uniud.easyhome.network.Node;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
+import org.icefaces.application.PortableRenderer;
 import org.icefaces.application.PushRenderer;
 
 @ManagedBean
-@ViewScoped 
-public class NodesController {
+@SessionScoped 
+public class NodesController implements Runnable {
     
     private static final String PUSH_GROUP = "nodes";
     
     private Random rnd;
     
+    private volatile boolean stopped;
+    
+    private PortableRenderer pRenderer;
+    
 	@EJB
 	private NetworkEJB networkEjb;
-
-    //@ManagedProperty(value="#{nodes}")
-    //private NodeList nodes = new NodeList();
-
+	
     @PostConstruct
     public void init() {
     	rnd = new Random();
         PushRenderer.addCurrentView(PUSH_GROUP);
+        Thread thr = new Thread(this);
+        thr.start();
+        pRenderer = PushRenderer.getPortableRenderer();
     }
     
     public List<Node> getNodes() {
@@ -56,9 +63,27 @@ public class NodesController {
     public int getSize() {
         return networkEjb.getNodes().size();
     }
-    
-    /*public void setNodes(NodeList nodes) {
-        this.nodes = nodes;
-    }*/
+
+	@Override
+	public void run() {
+		try {
+			
+			while(!stopped) {
+				Thread.sleep(1000);
+		    	Node node = new Node.Builder(rnd.nextLong())
+				.setAddress((short)(rnd.nextInt() & 0xFFFF))
+				.setGatewayId((byte)rnd.nextInt(255))
+				.setCapability((byte)rnd.nextInt(255))
+				.build();
+		    	networkEjb.insertOrUpdateNode(node);
+		    	pRenderer.render(PUSH_GROUP);
+			}	
+		} catch (InterruptedException ex) { }
+		
+	}
+	
+	public void doStop() {
+		stopped = true;
+	}
 
 }   
