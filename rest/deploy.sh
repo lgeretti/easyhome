025e2ce44abef4875c80212a90dfba4479b6579d
Luca Geretti <luca.geretti@gmail.com>
2013-03-20 19:26:30 +0100

    Generalized deployment script to account for host

--- rest/deploy.sh
+++ rest/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mode=$1
+host=$1
+mode=$2
 
 if [[ $mode == 'redeploy' ]]
 then
   echo "Redeploying..."
-  asadmin --user admin --host easyhome --port 4848 redeploy --name easyhome --dropandcreatetables=true target/easyhome-rest-0.0.1-SNAPSHOT.war
+  asadmin --user admin --host $host --port 4848 redeploy --name easyhome --dropandcreatetables=true target/easyhome-rest-0.0.1-SNAPSHOT.war
 else
   echo "Deploying..."
-  asadmin --user admin --host easyhome --port 4848 deploy --name easyhome --contextroot /easyhome --dropandcreatetables=true target/easyhome-rest-0.0.1-SNAPSHOT.war
+  asadmin --user admin --host $host --port 4848 deploy --name easyhome --contextroot /easyhome --dropandcreatetables=true target/easyhome-rest-0.0.1-SNAPSHOT.war
 fi
