1be4032c3d83ee094d1074d613b0b2bb841e8cb8
Luca Geretti <luca.geretti@gmail.com>
2013-03-22 13:46:26 +0100

    Initial test for XML parsing of actuators output

--- rest/src/test/java/it/uniud/easyhome/rest/SIPROGatewayTest.java
+++ rest/src/test/java/it/uniud/easyhome/rest/SIPROGatewayTest.java
@@ -21,7 +21,14 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
-@Ignore
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 public class SIPROGatewayTest {
 	
 	private static final String TARGET = "http://localhost:5000/";
@@ -53,6 +60,44 @@ public class SIPROGatewayTest {
 		String responseString = response.getEntity(String.class);
 		
 		assertEquals(ClientResponse.Status.OK,response.getClientResponseStatus());        
+	}
+	
+	@Test
+	public void readXml() {
+		
+	    try {
+	    	 
+	    	File fXmlFile = new File("/home/geretti/Public/sources/uniud/easyhome/rest/src/test/resources/actuators.xml");
+	    	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	    	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	    	Document doc = dBuilder.parse(fXmlFile);
+
+	    	//doc.getDocumentElement().normalize();
+	     
+	    	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+	     
+	    	NodeList nList = doc.getElementsByTagName("output1");
+	     
+	    	System.out.println("----------------------------");
+	     
+	    	for (int temp = 0; temp < nList.getLength(); temp++) {
+	     
+	    		Node nNode = nList.item(temp);
+	     
+	    		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+	     
+	    		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	     
+	    			Element eElement = (Element) nNode;
+	     
+	    			System.out.println("State : " + eElement.getElementsByTagName("state").item(0).getTextContent());
+	     
+	    		}
+	    	}
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }		
+		
 	}
 	
 }
