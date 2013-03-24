b5a8433ea1f1f4cb0e02fc105e0044cb219e989c
Luca Geretti <luca.geretti@gmail.com>
2013-03-24 23:33:10 +0100

    Fixed posting (putting, really) occupancy changes

--- rest/src/main/java/it/uniud/easyhome/processing/OccupancySensingRegistrationProcess.java
+++ rest/src/main/java/it/uniud/easyhome/processing/OccupancySensingRegistrationProcess.java
@@ -83,9 +83,8 @@ public class OccupancySensingRegistrationProcess extends Process {
 		        				
 		        				boolean previouslyOccupied = location.isOccupied();
 		        				location.setOccupied(occupied);
-				                restResource.path(RestPaths.LOCATIONS).path(Integer.toString(location.getId())).path("occupied").post(ClientResponse.class);
-				                restResource.path(RestPaths.STATES).path("sensors").path("presence").path(Integer.toString(location.getId())).path("occupied").post(ClientResponse.class);
-				                
+				                restResource.path(RestPaths.LOCATIONS).path(Integer.toString(location.getId())).path(occupied ? "occupied" : "unoccupied").put(ClientResponse.class);
+				                restResource.path(RestPaths.STATES).path("sensors").path("presence").path(Integer.toString(location.getId())).path(occupied ? "occupied" : "unoccupied").put(ClientResponse.class);
 				                
 				                if (previouslyOccupied != occupied) 
 				                	log(LogLevel.INFO, location + " is now " + (occupied ? "occupied" : "unoccupied"));
