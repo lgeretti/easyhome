daa7b252421242208a300030802f0b305cb8606d
Luca Geretti <luca.geretti@gmail.com>
2013-04-07 09:55:50 +0200

    Added specific icepush example

---
+++ icepush-example/src/main/java/it/uniud/icepush/counter/Counter.java
@@ -0,0 +1,19 @@
+package it.uniud.icepush.counter;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(eager=true)
+@ApplicationScoped
+public class Counter {
+
+    private int count;
+    
+    public synchronized void doIncrease() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
---
+++ icepush-example/src/main/java/it/uniud/icepush/counter/PushController.java
@@ -0,0 +1,37 @@
+package it.uniud.icepush.counter;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.icefaces.application.PushRenderer;
+
+@ManagedBean
+@ViewScoped 
+public class PushController {
+    
+    private static final String PUSH_GROUP = "counter";
+
+    @ManagedProperty(value="#{counter}")
+    private Counter counter;
+
+    @PostConstruct
+    public void init() {
+        PushRenderer.addCurrentView(PUSH_GROUP);
+    }
+    
+    public void doIncreaseCounter() {        
+        counter.doIncrease();
+        PushRenderer.render(PUSH_GROUP);
+    }
+    
+    public int getCount() {
+        return counter.getCount();
+    }
+    
+    public void setCounter(Counter counter) {
+        this.counter = counter;
+    }
+
+}   
